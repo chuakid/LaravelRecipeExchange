fc55d1214f0c379739a8825b8e95ad9f74ab05d7
Kelvin Chua <chuakid@gmail.com>
2018-04-15 13:47:33 +0800

    initial commit

---
+++ database/migrations/2018_04_06_083721_create_recipes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecipesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('submitter');
+            $table->string('description');
+            $table->string('status');
+            $table->integer('timeneeded');
+            $table->integer('preptime');
+            $table->integer('servings');
+            $table->string('image');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recipes');
+    }
+}
